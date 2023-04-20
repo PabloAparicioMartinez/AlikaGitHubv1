3132cfa89b512df1799173c77d6e24f78cb7f40b
PabloAparicioMartinez <pablo.aparicio1996@gmail.com>
2023-04-20 18:48:56 +0200

    Revert "3. Tercer commit: el tercer commit de prueba."
    This reverts commit ffffcdc318d02fa67ac6578504bfa341f87eac78.

--- app/src/main/java/com/example/alikagithubv1/MainActivity.kt
+++ app/src/main/java/com/example/alikagithubv1/MainActivity.kt
@@ -9,6 +9,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("Esto estará en el segundo commit")
-        println("Esto estará en el tercer commit")
     }
 }
