7fb2255d195617e16f9d38c4471fcbc83d676271
PabloAparicioMartinez <pablo.aparicio1996@gmail.com>
2023-04-26 22:12:19 +0200

    7. Commit de "Cambio 0" a "Cambio B" desde el Branch_02.

--- app/src/main/java/com/example/alikagithubv1/MainActivity.kt
+++ app/src/main/java/com/example/alikagithubv1/MainActivity.kt
@@ -13,6 +13,6 @@ class MainActivity : AppCompatActivity() {
         println("Esto estará en el cuarto commit")
         println("Commit en Branch_02")
 
-        println("Cambio 0")
+        println("Cambio B (Branch_02)")
     }
 }
